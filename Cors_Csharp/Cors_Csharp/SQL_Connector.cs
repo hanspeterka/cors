9189109c1fd6d64e80bc48b55462e896d65b3426
Unknown <janmpeterka@gmail.com>
2018-09-13 16:43:46 +0200

    nic 2

--- Cors_Csharp/Cors_Csharp/SQL_Connector.cs
+++ Cors_Csharp/Cors_Csharp/SQL_Connector.cs
@@ -12,35 +12,35 @@ namespace Cors_Csharp
     public class SQL_Connector
     {
          
-            private MySqlConnection connection;
-            private string server;
-            private string database;
-            private string uid;
-            private string password;
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
 
-            //Constructor
-            public SQL_Connector()
-            {
-                Initialize();
-            }
+        //Constructor
+        public SQL_Connector()
+        {
+            Initialize();
+        }
 
-            //Initialize values
-            private void Initialize()
-            {
-                server = "b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-                database = "eqxcz2kk2ha68vfu";
-                uid = "t2z7d0zkf4b9m6wu";
-                password = "rdk1h2wz1d5eishy";
-  
-                string connectionString;
-                connectionString = "server=" + server + ";" + "database=" +
-                database + ";" + "uid=" + uid + ";" + "password=" + password + ";";
+        //Initialize values
+        private void Initialize()
+        {
+            server = "b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+            database = "eqxcz2kk2ha68vfu";
+            uid = "t2z7d0zkf4b9m6wu";
+            password = "rdk1h2wz1d5eishy";
 
-                connection = new MySqlConnection(connectionString);
-            }
+            string connectionString;
+            connectionString = "server=" + server + ";" + "database=" +
+            database + ";" + "uid=" + uid + ";" + "password=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
 
         //open connection to database
-            private bool OpenConnection()
+        private bool OpenConnection()
         {
             try
             {
@@ -79,7 +79,7 @@ namespace Cors_Csharp
 
         //Close connection
         private bool CloseConnection()
-          {
+        {
             try
             {
                 connection.Close();
@@ -92,10 +92,6 @@ namespace Cors_Csharp
             }
         }
 
-            //Insert statement
-            public void Insert()
-            {
-            }
 
         //Insert statement
         public void Read()
@@ -121,37 +117,117 @@ namespace Cors_Csharp
             }
         }
 
+        //Insert statement
+        public void InsertTestingDataAll()
+        {
+            string[] line_array = new string[] {""};
+            string query="";
+
+            // Read weater data
+            var reader = new StreamReader(File.OpenRead("input_data/weather_data.csv"));
+            while(!reader.EndOfStream)
+            {
+                line_array = reader.ReadLine().Split(';');
+
+                int year = 0;
+                if (!Int32.TryParse(line_array[0], out year)) { continue; }
+
+                int month = 0;
+                if (!Int32.TryParse(line_array[1], out month)) { continue; }
+
+                int day = 0;
+                if (!Int32.TryParse(line_array[2], out day)) { continue; }
+
+                int hour = 0;
+                if (!Int32.TryParse(line_array[3], out hour)) { continue; }
+
+                DateTime datetime_1 = new DateTime(year, month, day, hour, 0, 0, 0);
+
+                float temperature = 0;
+                if (!float.TryParse(line_array[5], out temperature)) { continue; }
+
+                float precipitation = 0;
+                if (!float.TryParse(line_array[6], out precipitation)) { continue; }
+
+
+                query = "INSERT INTO weather (datetime, temperature, precipitation) VALUES ('" + datetime_1 + "','" + temperature + "', '" + precipitation + "');";
+                // Insert to database
+                if (this.OpenConnection() == true)
+                    try
+                    {
+                        MySqlCommand cmd_weather = new MySqlCommand(query, connection);
+                        cmd_weather.ExecuteNonQuery();
+                        Console.WriteLine("Weather data inserted.")
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+
+                this.CloseConnection();
+            }
+
+            reader = new StreamReader(File.OpenRead("input_data/steps_data.csv"));
+
+            while(!reader.EndOfStream)
+            {
+                line_array = reader.ReadLine().Split(',');
+
+                int steps_amount = 0;
+                if (!Int32.TryParse(line_array[13], out steps_amount)) { continue; }
+
+                DateTime datetime_2 = Convert.ToDateTime(line_array[0]).Date;
+
+                query = "INSERT INTO steps (date, steps_amount) VALUES ('" + datetime_2 + "', '" + steps_amount + "');";
+
+                // Insert into database
+                if (this.OpenConnection() == true)
+                    try
+                    {
+                        MySqlCommand cmd_steps = new MySqlCommand(query, connection);
+                        cmd_steps.ExecuteNonQuery();
+                        Console.WriteLine("Steps data inserted.")
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+
+                this.CloseConnection();
+
+            }
+
+
+        }
         //Update statement
         public void Update()
-            {
-            }
+        {
+        }
 
-            //Delete statement
-            public void Delete()
-            {
-            }
+        //Delete statement
+        public void Delete()
+        {
+        }
 
-            //Select statement
-            //public List<string>[] Select()
-            //{
-            //}
+        //Select statement
+        //public List<string>[] Select()
+        //{
+        //}
 
-            //Count statement
-            //public int Count()
-            //{
-            //}
+        //Count statement
+        //public int Count()
+        //{
+        //}
 
-            //Backup
-            //public void Backup()
-            //{
-            //}
+        //Backup
+        //public void Backup()
+        //{
+        //}
 
-            //Restore
-            public void Restore()
-            {
-            }
+        //Restore
+        public void Restore()
+        {
+        }
         
-
-
     }
 }
